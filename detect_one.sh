8925018c5e566f75f21b6782533308514ad58a9a
jovyan <jovanprodanov123@gmail.com>
2023-12-11 20:19:38 +0100

    add clean up

--- detect_one.sh
+++ detect_one.sh
@@ -7,6 +7,8 @@ file=$(find data -name "$record_id.hea" -print -quit)
 freq=$(head -n 1 "$file" | cut -d ' ' -f 3)
 filename=$(basename "$file" .hea)
 
+# cleanup
+rm -rf asc_data/*
 mkdir -p asc_data
 
 echo "Record name: $filename, file: $file, Frequency: $freq"
