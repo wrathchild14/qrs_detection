551da00b14c60439cf4ac7b9e298d986022920a6
jovyan <jovanprodanov123@gmail.com>
2023-12-11 20:02:40 +0100

    add testing script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+./detect_one.sh s20111
+./detect_one.sh s20121
+./detect_one.sh s20151
+./detect_one.sh s20171
+./detect_one.sh 202
+./detect_one.sh 207
+./detect_one.sh 208
+./detect_one.sh 220
